f74983178536d325ccca119d0e27172a1613fb5b
Jefersson Nathan <admin@phpse.net>
2016-05-29 00:04:45 -0300

    prototype hanoi game aggregate

---
+++ src/Domain/Aggregate/HanoiGame.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hanoi\Domain\Aggregate;
+
+use Hanoi\Domain\DomainEvent\NewGameWasCreated;
+use Prooph\EventSourcing\AggregateRoot;
+use Rhumsaa\Uuid\Uuid;
+
+final class HanoiGame extends AggregateRoot
+{
+    /**
+     * @var Uuid
+     */
+    private $uuid;
+
+    /**
+     * @var int
+     */
+    private $quantityOfPieces;
+
+    public static function newGameFromQuantityOfPieces(int $quantity) : self
+    {
+        $aggregate = new self();
+
+        $aggregate->uuid             = Uuid::uuid4();
+        $aggregate->quantityOfPieces = $quantity;
+
+        $aggregate->recordThat(
+            NewGameWasCreated::fromQuantityOfPieces($quantity)
+        );
+    }
+
+    public function movePieceToColumn()
+    {
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function aggregateId() : string
+    {
+        return (string) $this->uuid;
+    }
+}
