6a28a3edf2261ffc040d898e8c2e6e7edac99787
Jefersson Nathan <admin@phpse.net>
2016-05-30 17:21:11 -0300

    Command bus

---
+++ src/Factory/Services/CommandBus.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hanoi\Factory\Services;
+
+use Interop\Container\ContainerInterface;
+use Prooph\Common\Event\ActionEvent;
+use Prooph\Common\Event\ActionEventEmitter;
+use Prooph\Common\Event\ActionEventListenerAggregate;
+use Prooph\ServiceBus\MessageBus;
+use Prooph\ServiceBus\Plugin\ServiceLocatorPlugin;
+
+final class CommandBus
+{
+    public function __invoke(ContainerInterface $container) : \Prooph\ServiceBus\CommandBus
+    {
+        $commandBus = new \Prooph\ServiceBus\CommandBus();
+
+        $commandBus->utilize(new ServiceLocatorPlugin($container));
+
+        $commandBus->utilize($this->buildCommandRouter());
+
+        return $commandBus;
+    }
+
+    private function buildCommandRouter() : ActionEventListenerAggregate
+    {
+        return new class implements ActionEventListenerAggregate {
+            public function attach(ActionEventEmitter $dispatcher)
+            {
+                $dispatcher->attachListener(MessageBus::EVENT_ROUTE, [$this, 'onRoute']);
+            }
+
+            public function detach(ActionEventEmitter $dispatcher)
+            {
+                throw new \BadMethodCallException('Not implemented');
+            }
+
+            public function onRoute(ActionEvent $actionEvent)
+            {
+                $actionEvent->setParam(
+                    MessageBus::EVENT_PARAM_MESSAGE_HANDLER,
+                    (string) $actionEvent->getParam(MessageBus::EVENT_PARAM_MESSAGE_NAME)
+                );
+            }
+        };
+    }
+}
