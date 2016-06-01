91a9660984d68a0c205a16035d49cad67796cdc5
Jefersson Nathan <admin@phpse.net>
2016-06-01 13:52:30 -0300

    create command to check in

---
+++ src/Domain/Command/CheckIn.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hanoi\Domain\Command;
+
+use Prooph\Common\Messaging\Command;
+
+final class CheckIn extends Command
+{
+    /**
+     * @var \DateTimeImmutable
+     */
+    private $currentTime;
+
+    /**
+     * @var string
+     */
+    private $username;
+
+    private function __construct(string $username)
+    {
+        $this->init();
+
+        $this->currentTime = new \DateTimeImmutable();
+        $this->username    = $username;
+    }
+
+    public static function fromUserName(string $username) : self
+    {
+        return new static($username);
+    }
+
+    public function username() : string
+    {
+        return $this->username;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function payload() : array
+    {
+        return [
+            'username'    => $this->username,
+            'currentTime' => $this->currentTime->format('U'),
+        ];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setPayload(array $payload)
+    {
+        $this->currentTime = \DateTimeImmutable::createFromFormat('U', $payload['currentTime']);
+        $this->username    = $payload['username'];
+    }
+}
