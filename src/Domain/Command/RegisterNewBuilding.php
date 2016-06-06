50472240be442087ccd08aad6d89b33bd66c796f
Jefersson Nathan <admin@phpse.net>
2016-06-06 12:54:28 -0300

    add register new building command

---
+++ src/Domain/Command/RegisterNewBuilding.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hanoi\Domain\Command;
+
+use Prooph\Common\Messaging\Command;
+
+final class RegisterNewBuilding extends Command
+{
+    /**
+     * @var string
+     */
+    private $name;
+
+    private function __construct(string $name)
+    {
+        $this->init();
+
+        $this->name = $name;
+    }
+
+    public static function fromName(string $name) : self
+    {
+        return new self($name);
+    }
+
+    public function name() : string
+    {
+        return $this->name;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function payload() : array
+    {
+        return [
+            'name' => $this->name,
+        ];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function setPayload(array $payload)
+    {
+        $this->name = $payload['name'];
+    }
+}
