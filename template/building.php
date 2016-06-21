506af8fbd96d56a8a6b56803469004cd7bdb65b7
Marco Pivetta <ocramius@gmail.com>
2016-06-22 00:08:09 +0200

    Correcting usage of the `buildingId` (no longer a query parameter)

--- template/building.php
+++ template/building.php
@@ -1,15 +1,15 @@
-<?php /* @var \Rhumsaa\Uuid\Uuid $buildId */ ?>
+<?php /* @var \Rhumsaa\Uuid\Uuid $buildingId */ ?>
 <h1>Welcome to CQRS+ES building</h1>
 
 <h2>Check In: </h2>
-<form action="/checkin?id=<?= $buildingId; ?>" method="post">
+<form action="/checkin/<?= $buildingId; ?>" method="post">
     <input type="text" name="username" placeholder="Enter your username to checkin" required="required"/>
 
     <button>CheckIn</button>
 </form>
 
 <h2>Check Out: </h2>
-<form action="/checkout?id=<?= $buildingId; ?>" method="post">
+<form action="/checkout/<?= $buildingId; ?>" method="post">
     <input type="text" name="username" placeholder="Enter your username to checkout" required="required"/>
 
     <button>CheckOut</button>
