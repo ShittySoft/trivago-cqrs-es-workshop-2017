2b50cfb6623ea25839c5e8ac5520297938bec95b
Marco Pivetta <ocramius@gmail.com>
2016-06-23 03:49:19 +0200

    Inlining command bus factory

--- public/index.php
+++ public/index.php
@@ -136,8 +136,37 @@ call_user_func(function () {
                 return $eventStore;
             },
 
-            // Services
-            CommandBus::class                  => CommandBusFactory::class,
+            CommandBus::class                  => function (ContainerInterface $container) : CommandBus {
+                $commandBus = new CommandBus();
+
+                $commandBus->utilize(new \Prooph\ServiceBus\Plugin\ServiceLocatorPlugin($container));
+                $commandBus->utilize(new class implements ActionEventListenerAggregate {
+                    public function attach(ActionEventEmitter $dispatcher)
+                    {
+                        $dispatcher->attachListener(MessageBus::EVENT_ROUTE, [$this, 'onRoute']);
+                    }
+
+                    public function detach(ActionEventEmitter $dispatcher)
+                    {
+                        throw new \BadMethodCallException('Not implemented');
+                    }
+
+                    public function onRoute(ActionEvent $actionEvent)
+                    {
+                        $actionEvent->setParam(
+                            MessageBus::EVENT_PARAM_MESSAGE_HANDLER,
+                            (string) $actionEvent->getParam(MessageBus::EVENT_PARAM_MESSAGE_NAME)
+                        );
+                    }
+                });
+
+                $transactionManager = new \Prooph\EventStoreBusBridge\TransactionManager();
+                $transactionManager->setUp($container->get(EventStore::class));
+
+                $commandBus->utilize($transactionManager);
+
+                return $commandBus;
+            },
 
             // Command -> CommandHandlerFactory
             Command\RegisterNewBuilding::class => CommandHandlerFactory\RegisterNewBuildingHandlerFactory::class,
