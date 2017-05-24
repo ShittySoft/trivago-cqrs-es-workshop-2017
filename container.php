84f3afde340da54dc22bd5ce3bb87895c74921b4
Marco Pivetta <ocramius@gmail.com>
2017-05-24 11:21:14 +0200

    Command handler for when a check-in happens

--- container.php
+++ container.php
@@ -199,6 +199,17 @@ return new ServiceManager([
                 $buildings->add(Building::new($command->name()));
             };
         },
+        Command\CheckInUser::class => function (ContainerInterface $container) : callable {
+            $buildings = $container->get(BuildingRepositoryInterface::class);
+
+            return function (Command\CheckInUser $command) use ($buildings) {
+                $building = $buildings->get($command->buildingId());
+
+                $building->checkInUser($command->username());
+
+                $buildings->add($building);
+            };
+        },
         BuildingRepositoryInterface::class => function (ContainerInterface $container) : BuildingRepositoryInterface {
             return new BuildingRepository(
                 new AggregateRepository(
